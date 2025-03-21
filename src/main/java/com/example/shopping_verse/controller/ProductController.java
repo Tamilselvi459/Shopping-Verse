490c9d2066c402f2dc2d00cfda5721188e989e96
Your Name <you@example.com>
2025-03-21 19:37:56 +0530

    2c

--- src/main/java/com/example/shopping_verse/controller/ProductController.java
+++ src/main/java/com/example/shopping_verse/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+// product
 
 import java.util.List;
 
@@ -43,3 +44,4 @@ public class ProductController {
     }
 
 }
+
